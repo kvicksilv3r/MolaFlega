08300a18f6176dac9bd7cb432a8d00f18e362a23
Dogge <douglas@geoguessr.com>
2025-09-16 14:04:16 +0200

    Fix ability to paint on results screen
    lol

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,376 +7,381 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    public FlagContext currentContext;
-    public Transform buttonHolster;
-    public GameObject resultsHolster;
+	public FlagContext currentContext;
+	public Transform buttonHolster;
+	public GameObject resultsHolster;
 
-    public TextMeshProUGUI countryNameTMP;
-    public TextMeshProUGUI resultsTextTMP;
+	public TextMeshProUGUI countryNameTMP;
+	public TextMeshProUGUI resultsTextTMP;
 
-    public GameObject resultsImage;
+	public GameObject resultsImage;
 
-    public Painter painter;
+	public Painter painter;
 
-    public RawImage targetImage;
+	public RawImage targetImage;
 
-    public Slider thicknessSlider;
+	public Slider thicknessSlider;
 
-    public static GameManager Instance;
+	public static GameManager Instance;
 
-    private FlagContext[] contextDB;
-    private FlagContext[] shuffledSubDB;
+	private FlagContext[] contextDB;
+	private FlagContext[] shuffledSubDB;
 
-    private int currentContextIndex = 0;
+	private int currentContextIndex = 0;
 
-    public Transform thicknessPreview;
+	public Transform thicknessPreview;
 
-    public float thicknessMaxSize = 0.5f;
+	public float thicknessMaxSize = 0.5f;
 
-    public GameObject donePaintingButton;
-    public GameObject nextRoundButton;
+	public GameObject donePaintingButton;
+	public GameObject nextRoundButton;
 
-    public GameObject interactionsHolster;
-    public GameObject footer;
+	public GameObject interactionsHolster;
+	public GameObject footer;
 
-    public GameObject menuCanvas;
-    public GameObject gameCanvas;
+	public GameObject menuCanvas;
+	public GameObject gameCanvas;
 
-    public float canvasRefWidth;
-    public float canvasRefHeight;
+	public float canvasRefWidth;
+	public float canvasRefHeight;
 
-    public float resultRefWidth;
-    public float resultRefHeight;
+	public float resultRefWidth;
+	public float resultRefHeight;
 
-    public float intendedFlagWidth = 300;
-    public float intendedFlagHeight = 200;
+	public float intendedFlagWidth = 300;
+	public float intendedFlagHeight = 200;
 
-    public Button undoButton;
+	public Button undoButton;
 
-    public GameObject paintCanvas;
+	public GameObject paintCanvas;
 
-    private int lastFlagID = 999999;
+	private int lastFlagID = 999999;
 
-    public int similarityPercent;
+	public int similarityPercent;
 
-    public bool forceLoadCountryButton = false;
-    public FlagContext manualCountrySelection;
-
-    public List<Continent> continentFilter = new List<Continent>();
-
-    private void Awake()
-    {
-        if (!Instance)
-        {
-            Instance = this;
-        }
-
-        if (Instance != this)
-        {
-            Destroy(this);
-        }
-    }
-
-    public void Setup(FlagContext context)
-    {
-        SetupButtons();
-        painter.InitPainter(context.visualFlag.width, context.visualFlag.height);
-        HideResults();
-        SetText(context.countryName);
-    }
-
-    private void HideResults()
-    {
-        resultsHolster.SetActive(false);
-        resultsImage.SetActive(false);
-    }
-
-    public void UndoPaint()
-    {
-        painter.PopPaintStack();
-    }
-
-    public void NewCanvas()
-    {
-        painter.NewPainting();
-    }
-
-    public void ReloadScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-
-    private void SetTargetImage()
-    {
-        targetImage.texture = currentContext.visualFlag;
-    }
-
-    private void SetText(string countryName)
-    {
-        countryNameTMP.text = countryName;
-    }
-
-    public void DonePainting()
-    {
-        CompareFlags();
-        SetTargetImage();
-        DisplayResults();
-        donePaintingButton.SetActive(false);
-        nextRoundButton.SetActive(true);
-    }
-
-    private void DisplayResults()
-    {
-        resultsHolster.SetActive(true);
-        resultsTextTMP.text = $"{similarityPercent}%";
-        resultsImage.SetActive(true);
-        interactionsHolster.SetActive(false);
-        footer.SetActive(false);
-    }
-
-    internal void UpdateColor(Color color)
-    {
-        painter.SetColor(color);
-    }
-
-    private void Update()
-    {
-        undoButton.interactable = (painter.GetPaintStackSize() > 1);
-    }
-
-    private void LoadResources()
-    {
-        contextDB = Resources.LoadAll<FlagContext>("FlagObjects");
-    }
-
-    private void SetupButtons()
-    {
-        //Setup all colors
-        for (int i = 0; i < buttonHolster.childCount; i++)
-        {
-            var b = buttonHolster.GetChild(i);
-
-            if (i < currentContext.colors.Length)
-            {
-                b.gameObject.SetActive(true);
-                b.gameObject.GetComponent<ColorButton>().SetColor(currentContext.colors[i]);
-            }
-            else
-            {
-                b.gameObject.SetActive(false);
-            }
-        }
-
-        //Set default color
-        buttonHolster.GetChild(0).GetComponent<ColorButton>().ClickedOn();
-    }
-
-    public void ClearCanvas()
-    {
-        painter.ClearCanvas();
-    }
-
-    public void NewRound()
-    {
-        //LoadRandomFlag();
-        LoadNextFlag();
-
-        SetCanvasPhysicalSize();
-        SetResultsPhysicalSize();
-
-        Setup(currentContext);
-        interactionsHolster.SetActive(true);
-        donePaintingButton.SetActive(true);
-        nextRoundButton.SetActive(false);
-        footer.SetActive(true);
-    }
-    private void LoadNextFlag()
-    {
-        var index = ((currentContextIndex++) % shuffledSubDB.Length);
-        currentContext = shuffledSubDB[index];
-    }
-
-    private void SetupFlagOrder()
-    {
-        shuffledSubDB = contextDB.Where(c => continentFilter.Contains(c.countryContinent)).ToArray();
-        System.Random rand = new System.Random();
-        shuffledSubDB = shuffledSubDB.OrderBy(c => rand.Next()).ToArray();
-    }
-
-    private void SetCanvasPhysicalSize()
-    {
-        var width = currentContext.visualFlag.width * (canvasRefWidth / intendedFlagWidth);
-        var height = currentContext.visualFlag.height * (canvasRefHeight / intendedFlagHeight);
-
-        paintCanvas.transform.localScale = new Vector3(width, 1, height);
-    }
-
-    private void SetResultsPhysicalSize()
-    {
-        var width = currentContext.visualFlag.width * (resultRefWidth / intendedFlagWidth);
-        var height = currentContext.visualFlag.height * (resultRefHeight / intendedFlagHeight);
-
-        resultsImage.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
-    }
-
-
-    public void ManualSetup()
-    {
-        if (!currentContext)
-        {
-            return;
-        }
-
-        Setup(currentContext);
-    }
-
-    public void LoadRandomFlag()
-    {
-        var subDB = contextDB.Where(c => continentFilter.Contains(c.countryContinent)).ToList();
-        currentContext = subDB[GetRandomFlagIndex(subDB.Count)];
-        //currentContext = contextDB[GetRandomFlagIndex(contextDB.Length)];
-    }
-
-    public int GetRandomFlagIndex(int maxIndex)
-    {
-        var rngFlagIndex = Random.Range(0, maxIndex);
-
-        if (rngFlagIndex == lastFlagID)
-        {
-            rngFlagIndex = GetRandomFlagIndex(maxIndex);
-        }
-
-        lastFlagID = rngFlagIndex;
-
-        return rngFlagIndex;
-    }
-
-    public void SkipRound()
-    {
-        NewRound();
-    }
-
-    public void CompareFlags()
-    {
-        var a = painter.mainPaintTexture;
-        var b = currentContext.visualFlag;
-
-        if (a.width != b.width)
-        {
-            print("The images are not the same width");
-            return;
-        }
-
-        if (a.height != b.height)
-        {
-            print("The images are not the same height");
-            return;
-        }
-
-        int matches = 0;
-        int badPixels = 0;
-        Color aColor;
-        Color bColor;
-
-        for (int width = 0; width < a.width; width++)
-        {
-            for (int height = 0; height < a.height; height++)
-            {
-                aColor = a.GetPixel(width, height);
-                bColor = b.GetPixel(width, height);
-
-                if (a.GetPixel(width, height) == b.GetPixel(width, height))
-                {
-                    matches++;
-                }
-                else
-                {
-                    var targetPixel = b.GetPixel(width, height);
-                    var colorMatch = 0;
-                    for (int x = 0; x < currentContext.colors.Length; x++)
-                    {
-                        if (currentContext.colors[x] == targetPixel)
-                        {
-                            colorMatch++;
-                        }
-                    }
-
-                    if (colorMatch == 0)
-                    {
-                        badPixels++;
-                    }
-                }
-            }
-        }
-
-        var totalPixels = (a.width * a.height) - badPixels;
-        float matchPercent = (float)matches / (float)totalPixels;
-        similarityPercent = (int)Mathf.Round(matchPercent * 100);
-        print($"The images were {similarityPercent}% similar. Sheesh");
-    }
-
-    public void ThicknessSliderChanged()
-    {
-        painter.SetBrushThickness((int)thicknessSlider.value);
-        SetThicknessPreview();
-    }
-
-    private void SetThicknessPreview()
-    {
-        var newSize = thicknessMaxSize * (thicknessSlider.value / thicknessSlider.maxValue);
-        thicknessPreview.localScale = new Vector3(newSize, newSize, 1);
-    }
-
-    public bool ToggleContinent(Continent continent)
-    {
-        if (continentFilter.Contains(continent))
-        {
-            continentFilter.Remove(continent);
-            return false;
-        }
-
-        continentFilter.Add(continent);
-        return true;
-    }
-
-    public bool ContainsContinent(Continent continent)
-    {
-        return continentFilter.Contains(continent);
-    }
-
-    private void OnValidate()
-    {
-        if (forceLoadCountryButton && manualCountrySelection != null)
-        {
-            forceLoadCountryButton = false;
-
-            currentContext = manualCountrySelection;
-
-            SetCanvasPhysicalSize();
-            SetResultsPhysicalSize();
-
-            Setup(currentContext);
-            interactionsHolster.SetActive(true);
-            donePaintingButton.SetActive(true);
-            nextRoundButton.SetActive(false);
-        }
-    }
-
-    public void StartGame()
-    {
-        if (continentFilter.Count == 0)
-        {
-            return;
-        }
-
-        gameCanvas.SetActive(true);
-        paintCanvas.SetActive(true);
-        menuCanvas.SetActive(false);
-
-        painter.InitPainter(300, 200);
-        LoadResources();
-        SetupFlagOrder();
-
-        NewRound();
-    }
+	public bool forceLoadCountryButton = false;
+	public FlagContext manualCountrySelection;
+
+	public List<Continent> continentFilter = new List<Continent>();
+
+	private void Awake()
+	{
+		if (!Instance)
+		{
+			Instance = this;
+		}
+
+		if (Instance != this)
+		{
+			Destroy(this);
+		}
+	}
+
+	public void Setup(FlagContext context)
+	{
+		SetupButtons();
+		painter.InitPainter(context.visualFlag.width, context.visualFlag.height);
+		HideResults();
+		SetText(context.countryName);
+	}
+
+	private void HideResults()
+	{
+		resultsHolster.SetActive(false);
+		resultsImage.SetActive(false);
+	}
+
+	public void UndoPaint()
+	{
+		painter.PopPaintStack();
+	}
+
+	public void NewCanvas()
+	{
+		painter.NewPainting();
+	}
+
+	public void ReloadScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	private void SetTargetImage()
+	{
+		targetImage.texture = currentContext.visualFlag;
+	}
+
+	private void SetText(string countryName)
+	{
+		countryNameTMP.text = countryName;
+	}
+
+	public void DonePainting()
+	{
+		CompareFlags();
+		SetTargetImage();
+		DisplayResults();
+		donePaintingButton.SetActive(false);
+		nextRoundButton.SetActive(true);
+	}
+
+	private void DisplayResults()
+	{
+		resultsHolster.SetActive(true);
+		resultsTextTMP.text = $"{similarityPercent}%";
+		resultsImage.SetActive(true);
+		interactionsHolster.SetActive(false);
+		footer.SetActive(false);
+	}
+
+	internal void UpdateColor(Color color)
+	{
+		painter.SetColor(color);
+	}
+
+	private void Update()
+	{
+		undoButton.interactable = (painter.GetPaintStackSize() > 1);
+	}
+
+	private void LoadResources()
+	{
+		contextDB = Resources.LoadAll<FlagContext>("FlagObjects");
+	}
+
+	private void SetupButtons()
+	{
+		//Setup all colors
+		for (int i = 0; i < buttonHolster.childCount; i++)
+		{
+			var b = buttonHolster.GetChild(i);
+
+			if (i < currentContext.colors.Length)
+			{
+				b.gameObject.SetActive(true);
+				b.gameObject.GetComponent<ColorButton>().SetColor(currentContext.colors[i]);
+			}
+			else
+			{
+				b.gameObject.SetActive(false);
+			}
+		}
+
+		//Set default color
+		buttonHolster.GetChild(0).GetComponent<ColorButton>().ClickedOn();
+	}
+
+	public void ClearCanvas()
+	{
+		painter.ClearCanvas();
+	}
+
+	public void NewRound()
+	{
+		//LoadRandomFlag();
+		LoadNextFlag();
+
+		SetCanvasPhysicalSize();
+		SetResultsPhysicalSize();
+
+		Setup(currentContext);
+		interactionsHolster.SetActive(true);
+		donePaintingButton.SetActive(true);
+		nextRoundButton.SetActive(false);
+		footer.SetActive(true);
+	}
+	private void LoadNextFlag()
+	{
+		var index = ((currentContextIndex++) % shuffledSubDB.Length);
+		currentContext = shuffledSubDB[index];
+	}
+
+	private void SetupFlagOrder()
+	{
+		shuffledSubDB = contextDB.Where(c => continentFilter.Contains(c.countryContinent)).ToArray();
+		System.Random rand = new System.Random();
+		shuffledSubDB = shuffledSubDB.OrderBy(c => rand.Next()).ToArray();
+	}
+
+	private void SetCanvasPhysicalSize()
+	{
+		var width = currentContext.visualFlag.width * (canvasRefWidth / intendedFlagWidth);
+		var height = currentContext.visualFlag.height * (canvasRefHeight / intendedFlagHeight);
+
+		paintCanvas.transform.localScale = new Vector3(width, 1, height);
+	}
+
+	private void SetResultsPhysicalSize()
+	{
+		var width = currentContext.visualFlag.width * (resultRefWidth / intendedFlagWidth);
+		var height = currentContext.visualFlag.height * (resultRefHeight / intendedFlagHeight);
+
+		resultsImage.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+	}
+
+
+	public void ManualSetup()
+	{
+		if (!currentContext)
+		{
+			return;
+		}
+
+		Setup(currentContext);
+	}
+
+	public void LoadRandomFlag()
+	{
+		var subDB = contextDB.Where(c => continentFilter.Contains(c.countryContinent)).ToList();
+		currentContext = subDB[GetRandomFlagIndex(subDB.Count)];
+		//currentContext = contextDB[GetRandomFlagIndex(contextDB.Length)];
+	}
+
+	public int GetRandomFlagIndex(int maxIndex)
+	{
+		var rngFlagIndex = Random.Range(0, maxIndex);
+
+		if (rngFlagIndex == lastFlagID)
+		{
+			rngFlagIndex = GetRandomFlagIndex(maxIndex);
+		}
+
+		lastFlagID = rngFlagIndex;
+
+		return rngFlagIndex;
+	}
+
+	public void SkipRound()
+	{
+		NewRound();
+	}
+
+	public void CompareFlags()
+	{
+		var a = painter.mainPaintTexture;
+		var b = currentContext.visualFlag;
+
+		if (a.width != b.width)
+		{
+			print("The images are not the same width");
+			return;
+		}
+
+		if (a.height != b.height)
+		{
+			print("The images are not the same height");
+			return;
+		}
+
+		int matches = 0;
+		int badPixels = 0;
+		Color aColor;
+		Color bColor;
+
+		for (int width = 0; width < a.width; width++)
+		{
+			for (int height = 0; height < a.height; height++)
+			{
+				aColor = a.GetPixel(width, height);
+				bColor = b.GetPixel(width, height);
+
+				if (a.GetPixel(width, height) == b.GetPixel(width, height))
+				{
+					matches++;
+				}
+				else
+				{
+					var targetPixel = b.GetPixel(width, height);
+					var colorMatch = 0;
+					for (int x = 0; x < currentContext.colors.Length; x++)
+					{
+						if (currentContext.colors[x] == targetPixel)
+						{
+							colorMatch++;
+						}
+					}
+
+					if (colorMatch == 0)
+					{
+						badPixels++;
+					}
+				}
+			}
+		}
+
+		var totalPixels = (a.width * a.height) - badPixels;
+		float matchPercent = (float)matches / (float)totalPixels;
+		similarityPercent = (int)Mathf.Round(matchPercent * 100);
+		print($"The images were {similarityPercent}% similar. Sheesh");
+	}
+
+	public void ThicknessSliderChanged()
+	{
+		painter.SetBrushThickness((int)thicknessSlider.value);
+		SetThicknessPreview();
+	}
+
+	private void SetThicknessPreview()
+	{
+		var newSize = thicknessMaxSize * (thicknessSlider.value / thicknessSlider.maxValue);
+		thicknessPreview.localScale = new Vector3(newSize, newSize, 1);
+	}
+
+	public bool ToggleContinent(Continent continent)
+	{
+		if (continentFilter.Contains(continent))
+		{
+			continentFilter.Remove(continent);
+			return false;
+		}
+
+		continentFilter.Add(continent);
+		return true;
+	}
+
+	public bool ContainsContinent(Continent continent)
+	{
+		return continentFilter.Contains(continent);
+	}
+
+	public bool IsInResultsView()
+	{
+		return resultsImage.gameObject.activeInHierarchy;
+	}
+
+	private void OnValidate()
+	{
+		if (forceLoadCountryButton && manualCountrySelection != null)
+		{
+			forceLoadCountryButton = false;
+
+			currentContext = manualCountrySelection;
+
+			SetCanvasPhysicalSize();
+			SetResultsPhysicalSize();
+
+			Setup(currentContext);
+			interactionsHolster.SetActive(true);
+			donePaintingButton.SetActive(true);
+			nextRoundButton.SetActive(false);
+		}
+	}
+
+	public void StartGame()
+	{
+		if (continentFilter.Count == 0)
+		{
+			return;
+		}
+
+		gameCanvas.SetActive(true);
+		paintCanvas.SetActive(true);
+		menuCanvas.SetActive(false);
+
+		painter.InitPainter(300, 200);
+		LoadResources();
+		SetupFlagOrder();
+
+		NewRound();
+	}
 }
--- Assets/Painter.cs
+++ Assets/Painter.cs
@@ -4,201 +4,206 @@ using UnityEngine;
 
 public class Painter : MonoBehaviour
 {
-    public Renderer planeRenderer;
+	public Renderer planeRenderer;
 
-    public Material planeMat;
+	public Material planeMat;
 
-    public Texture2D mainPaintTexture;
+	public Texture2D mainPaintTexture;
 
-    public Stack<Texture2D> paintStack = new Stack<Texture2D>();
+	public Stack<Texture2D> paintStack = new Stack<Texture2D>();
 
-    public int textureWidth = 300;
-    public int textureHeight = 200;
+	public int textureWidth = 300;
+	public int textureHeight = 200;
 
-    public bool testScratch = false;
+	public bool testScratch = false;
 
-    public Color paintColor;
+	public Color paintColor;
 
-    [Range(1, 20)]
-    public int paintThickness = 10;
+	[Range(1, 20)]
+	public int paintThickness = 10;
 
-    private bool holdingMouseButton = false;
-    private bool heldMouseButtonLastFrame = false;
-    private Vector2 lastHitCoord;
+	private bool holdingMouseButton = false;
+	private bool heldMouseButtonLastFrame = false;
+	private Vector2 lastHitCoord;
 
-    private Color[] pxls = new Color[65536];
+	private Color[] pxls = new Color[65536];
 
-    public int minDrawsOnDrag = 4;
-    public int magnitudeAmp = 100;
+	public int minDrawsOnDrag = 4;
+	public int magnitudeAmp = 100;
 
-    private bool pixelsChanged = false;
+	private bool pixelsChanged = false;
 
-    void Start()
-    {
-        Application.targetFrameRate = 60;
-    }
+	void Start()
+	{
+		Application.targetFrameRate = 60;
+	}
 
-    public void InitPainter(int textureWidth = 300, int textureHeight = 200)
-    {
-        this.textureWidth = textureWidth;
-        this.textureHeight = textureHeight;
+	public void InitPainter(int textureWidth = 300, int textureHeight = 200)
+	{
+		this.textureWidth = textureWidth;
+		this.textureHeight = textureHeight;
 
-        planeMat = planeRenderer.material;
-        CreateTexture();
-        AssignTexture();
-        ClearCanvas();
-        NewPainting();
-    }
+		planeMat = planeRenderer.material;
+		CreateTexture();
+		AssignTexture();
+		ClearCanvas();
+		NewPainting();
+	}
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            holdingMouseButton = true;
-        }
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			holdingMouseButton = true;
+		}
 
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            holdingMouseButton = false;
-            heldMouseButtonLastFrame = false;
+		if (Input.GetKeyUp(KeyCode.Mouse0))
+		{
+			holdingMouseButton = false;
+			heldMouseButtonLastFrame = false;
 
-            if (pixelsChanged)
-            {
-                PushPaintStack();
-                pixelsChanged = false;
-            }
-        }
+			if (pixelsChanged)
+			{
+				PushPaintStack();
+				pixelsChanged = false;
+			}
+		}
 
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-            DoRaycast();
-        }
-    }
+		if (Input.GetKey(KeyCode.Mouse0))
+		{
+			if (GameManager.Instance.IsInResultsView())
+			{
+				return;
+			}
 
-    private void DoRaycast()
-    {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			DoRaycast();
+		}
+	}
+
+	private void DoRaycast()
+	{
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
 
-        if (Physics.Raycast(ray, out hit))
-        {
-            if (mainPaintTexture != null)
-            {
-                int xCoord = (int)(hit.textureCoord.x * textureWidth);
-                int yCoord = (int)(hit.textureCoord.y * textureHeight);
+		if (Physics.Raycast(ray, out hit))
+		{
+			if (mainPaintTexture != null)
+			{
+				int xCoord = (int)(hit.textureCoord.x * textureWidth);
+				int yCoord = (int)(hit.textureCoord.y * textureHeight);
 
-                if (heldMouseButtonLastFrame)
-                {
-                    var magnitude = (int)(((lastHitCoord - hit.textureCoord) * magnitudeAmp).magnitude);
-                    if (magnitude < minDrawsOnDrag) { magnitude = minDrawsOnDrag; }
+				if (heldMouseButtonLastFrame)
+				{
+					var magnitude = (int)(((lastHitCoord - hit.textureCoord) * magnitudeAmp).magnitude);
+					if (magnitude < minDrawsOnDrag) { magnitude = minDrawsOnDrag; }
 
-                    //print(magnitude);
-                    for (int i = 0; i < magnitude; i++)
-                    {
-                        var lerpedHit = Vector2.Lerp(lastHitCoord, hit.textureCoord, (float)i / (float)magnitude);
-                        var x = (int)(lerpedHit.x * textureWidth);
-                        var y = (int)(lerpedHit.y * textureHeight);
-                        PaintPixels(x, y);
-                    }
-                }
+					//print(magnitude);
+					for (int i = 0; i < magnitude; i++)
+					{
+						var lerpedHit = Vector2.Lerp(lastHitCoord, hit.textureCoord, (float)i / (float)magnitude);
+						var x = (int)(lerpedHit.x * textureWidth);
+						var y = (int)(lerpedHit.y * textureHeight);
+						PaintPixels(x, y);
+					}
+				}
 
-                else
-                {
-                    PaintPixels(xCoord, yCoord);
-                }
-                lastHitCoord = hit.textureCoord;
-                heldMouseButtonLastFrame = true;
-            }
-        }
-    }
+				else
+				{
+					PaintPixels(xCoord, yCoord);
+				}
+				lastHitCoord = hit.textureCoord;
+				heldMouseButtonLastFrame = true;
+			}
+		}
+	}
 
-    private void PaintPixels(int xCoord, int yCoord)
-    {
-        mainPaintTexture.Circle(xCoord, yCoord, paintThickness, paintColor);
-        mainPaintTexture.Apply();
-        pixelsChanged = true;
-    }
+	private void PaintPixels(int xCoord, int yCoord)
+	{
+		mainPaintTexture.Circle(xCoord, yCoord, paintThickness, paintColor);
+		mainPaintTexture.Apply();
+		pixelsChanged = true;
+	}
 
-    public void SetBrushThickness(int thickness)
-    {
-        paintThickness = thickness;
-    }
+	public void SetBrushThickness(int thickness)
+	{
+		paintThickness = thickness;
+	}
 
-    private void AssignTexture()
-    {
-        if (mainPaintTexture == null)
-        {
-            print("Make a texture first");
-            return;
-        }
+	private void AssignTexture()
+	{
+		if (mainPaintTexture == null)
+		{
+			print("Make a texture first");
+			return;
+		}
 
-        planeMat.mainTexture = mainPaintTexture;
-    }
+		planeMat.mainTexture = mainPaintTexture;
+	}
 
-    private void CreateTexture()
-    {
-        MakeTexture();
-    }
+	private void CreateTexture()
+	{
+		MakeTexture();
+	}
 
-    private void MakeTexture()
-    {
-        mainPaintTexture = new Texture2D(textureWidth, textureHeight, TextureFormat.RGBA32, false);
-        ClearCanvas();
-    }
+	private void MakeTexture()
+	{
+		mainPaintTexture = new Texture2D(textureWidth, textureHeight, TextureFormat.RGBA32, false);
+		ClearCanvas();
+	}
 
-    public void NewPainting()
-    {
-        ClearPaintStack();
+	public void NewPainting()
+	{
+		ClearPaintStack();
 
-        ClearCanvas();
-    }
+		ClearCanvas();
+	}
 
-    public void ClearCanvas()
-    {
-        for (int w = 0; w < textureWidth; w++)
-        {
-            for (int h = 0; h < textureHeight; h++)
-            {
-                mainPaintTexture.SetPixel(w, h, Color.white);
-            }
-        }
+	public void ClearCanvas()
+	{
+		for (int w = 0; w < textureWidth; w++)
+		{
+			for (int h = 0; h < textureHeight; h++)
+			{
+				mainPaintTexture.SetPixel(w, h, Color.white);
+			}
+		}
 
-        mainPaintTexture.Apply();
+		mainPaintTexture.Apply();
 
-        PushPaintStack();
-    }
+		PushPaintStack();
+	}
 
-    public void SetColor(Color color)
-    {
-        paintColor = color;
-    }
+	public void SetColor(Color color)
+	{
+		paintColor = color;
+	}
 
-    private void PushPaintStack()
-    {
-        var newTexture = new Texture2D(textureWidth, textureHeight, TextureFormat.RGBA32, false);
-        newTexture.CopyPixels(mainPaintTexture);
-        newTexture.Apply();
-        paintStack.Push(newTexture);
-    }
+	private void PushPaintStack()
+	{
+		var newTexture = new Texture2D(textureWidth, textureHeight, TextureFormat.RGBA32, false);
+		newTexture.CopyPixels(mainPaintTexture);
+		newTexture.Apply();
+		paintStack.Push(newTexture);
+	}
 
-    public void PopPaintStack()
-    {
-        if (paintStack.Count > 1)
-        {
-            paintStack.Pop();
-            mainPaintTexture.CopyPixels(paintStack.Peek());
-            mainPaintTexture.Apply();
-        }
-    }
+	public void PopPaintStack()
+	{
+		if (paintStack.Count > 1)
+		{
+			paintStack.Pop();
+			mainPaintTexture.CopyPixels(paintStack.Peek());
+			mainPaintTexture.Apply();
+		}
+	}
 
-    private void ClearPaintStack()
-    {
-        paintStack.Clear();
-    }
+	private void ClearPaintStack()
+	{
+		paintStack.Clear();
+	}
 
-    public int GetPaintStackSize()
-    {
-        return paintStack.Count;
-    }
+	public int GetPaintStackSize()
+	{
+		return paintStack.Count;
+	}
 }
